542f4e2ff0467f73ac5b1ecd307a7da6494ca81f
thekingking <1677273255@qq.com>
2024-05-30 14:32:30 +0800

    finish assignment3

--- src/editor.rs
+++ src/editor.rs
@@ -3,6 +3,9 @@ mod terminal;
 use terminal::{Terminal, Size, Position};
 use std::io::Error;
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
 }
@@ -63,12 +66,37 @@ impl Editor {
         Ok(())
     }
 
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+        #[allow(clippy::integer_division)]
+        let padding = (width.saturating_sub(len)) / 2;
+
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(&welcome_message)?;
+        Ok(())
+    }
+
+    fn draw_empty_rows() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
     fn draw_rows() -> Result<(), Error> {
-        let Size{height, ..} = Terminal::size()?;
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
             Terminal::print("~")?;
-            if current_row + 1 < height {
+            #[allow(clippy::integer_division)]
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_rows()?;
+            }
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,19 +1,20 @@
 use crossterm::cursor::{MoveTo, Hide, Show};
-use crossterm::queue;
+use crossterm::{queue, Command};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
 use crossterm::style::Print;
 use std::io::{stdout, Write, Error};
+use core::fmt::Display;
 
 #[derive(Copy, Clone)]
 pub struct Size {
-    pub height: u16,
-    pub width: u16,
+    pub height: usize,
+    pub width: usize,
 }
 
 #[derive(Copy, Clone)]
 pub struct Position {
-    pub x: u16,
-    pub y: u16, 
+    pub x: usize,
+    pub y: usize, 
 }
 
 pub struct Terminal;
@@ -36,42 +37,54 @@ impl Terminal {
     }
 
     pub fn clear_screen() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::All))?;
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
     pub fn clear_line() -> Result<(), Error> {
-        queue!(stdout(), Clear(ClearType::CurrentLine))?;
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
+    /// 移动光标到指定位置
     pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        queue!(stdout(), MoveTo(position.x, position.y))?;
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
         Ok(())
     }
 
     pub fn hide_cursor() -> Result<(), Error> {
-        queue!(stdout(), Hide)?;
+        Self::queue_command(Hide)?;
         Ok(())
     }
 
     pub fn show_cursor() -> Result<(), Error> {
-        queue!(stdout(), Show)?;
+        Self::queue_command(Show)?;
         Ok(())
     }
 
-    pub fn print(string: &str) -> Result<(), Error> {
-        queue!(stdout(), Print(string))?;
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
         Ok(())
     }
 
+    /// 返回当前终端窗口大小
     pub fn size() -> Result<Size, Error> {
-        let (width, height) = size()?;
-        Ok(Size {width, height})
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        Ok(Size { width, height })
     }
 
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 }
